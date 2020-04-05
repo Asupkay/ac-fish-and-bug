74fc7b3abbcf52f5e4cda74db86d8b0925eda6b0
Alex Supkay <asupkay1124@gmail.com>
2020-04-05 10:23:36 -0400

    Updates

---
+++ precache-manifest.dabd2360fc4824061029797e0ace0de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e62c8cd1a36b190736018214c7cac3",
+    "url": "/ac-fish-and-bug/index.html"
+  },
+  {
+    "revision": "c11829491cbd35165861",
+    "url": "/ac-fish-and-bug/static/css/main.803db2ec.chunk.css"
+  },
+  {
+    "revision": "d5ac65dfbffce053dbc7",
+    "url": "/ac-fish-and-bug/static/js/2.110c5eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ac-fish-and-bug/static/js/2.110c5eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11829491cbd35165861",
+    "url": "/ac-fish-and-bug/static/js/main.95b6c20f.chunk.js"
+  },
+  {
+    "revision": "f4356824af6e6aacc6d2",
+    "url": "/ac-fish-and-bug/static/js/runtime-main.0954b70d.js"
+  }
+]);
